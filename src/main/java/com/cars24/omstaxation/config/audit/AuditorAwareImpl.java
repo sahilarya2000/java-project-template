7aa84d5b9728d9bee6da249621e1ebbf0b87ba73
Ajay Goyal <ajay.goyal@cars24.com>
2019-09-10 13:10:17 +0530

    adding auditing,service charge entity, flyway

---
+++ src/main/java/com/cars24/omstaxation/config/audit/AuditorAwareImpl.java
@@ -0,0 +1,14 @@
+package com.cars24.omstaxation.config.audit;
+
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("admin");
+    }
+}
