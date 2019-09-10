5b6424c9f655ba3c3faee034ecc6dd17bd173441
saunik singh <saunik.singh@cars24.com>
2019-09-10 17:05:10 +0530

    OJ-22- Service Implemented

--- src/main/java/com/cars24/omstaxation/service/impl/TaxConfigServiceImpl.java
+++ src/main/java/com/cars24/omstaxation/service/impl/TaxConfigServiceImpl.java
@@ -4,12 +4,16 @@ import com.cars24.omstaxation.entity.TaxConfig;
 import com.cars24.omstaxation.exception.TaxConfigNotFoundException;
 import com.cars24.omstaxation.repository.TaxConfigRepository;
 import com.cars24.omstaxation.service.TaxConfigService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 /**
  * @author sauniksingh
  */
+@Slf4j
+@Service
 public class TaxConfigServiceImpl implements TaxConfigService {
     @Autowired
     private TaxConfigRepository taxConfigRepository;
