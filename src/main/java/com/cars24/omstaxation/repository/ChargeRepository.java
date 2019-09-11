36b5d88753b2b624c4826567c08d733371281964
Ajay Goyal <ajay.goyal@cars24.com>
2019-09-11 10:31:33 +0000

    Revert "OJ-9 (pull request #4)"
    This reverts pull request #4.
    
    > * adding auditing,service charge entity, flyway
    > * adding flyway file and service charge entity
    > * OJ-22
    > * OJ-22- Added Controller
    > * OJ-22- Service Implemented
    > * OJ-22- DTO created
    > * adding crud for service charge
    > * OJ-22- DTO exception handle
    > * service charge crud bug fixes
    > * OJ-22- added add, update, get, delete
    > * OJ-22- message Changed
    > * adding validation in service charge crud
    > * OJ-22- Changed to state\_id
    > * validation added for service charge crud
    > * OJ-22- removed extra /

--- src/main/java/com/cars24/omstaxation/repository/ChargeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cars24.omstaxation.repository;
-
-import com.cars24.omstaxation.entity.Charge;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChargeRepository extends JpaRepository<Charge,Long> {
-}
